744403cb559b517b8a11e21c5ec0f2fbc97d2666
Linad <linad43merzlikin@yandex.com>
2026-01-12 18:06:14 +0300

    edit module

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,8 +1,6 @@
-import src.masks
+import src.widget
 
-print(src.masks.mask_account_card("Visa Platinum 7000792289606361"))
-print(src.masks.mask_account_card("Maestro 7000792289606361"))
-print(src.masks.mask_account_card("Счет 73654108430135874305"))
-print(src.masks.get_date("2018-07-11"))
-print(src.masks.get_date("11 July 2018"))
-print(src.masks.get_date("11/07/2018"))
+print(src.widget.mask_account_card("Visa Platinum 7000792289606361"))
+print(src.widget.mask_account_card("Maestro 7000792289606361"))
+print(src.widget.mask_account_card("Счет 73654108430135874305"))
+print(src.widget.get_date("2024-03-11T02:26:18.671407"))
--- PythonProject/src/widget.py
+++ PythonProject/src/widget.py
@@ -1,22 +1,28 @@
+import src.masks
+
+
 def mask_account_card(input_account_card: str) -> str:
-    """получение из полного наименования  счета или карточки,
+    """Получение из полного наименования  счета или карточки,
     только числа счета или карты
     и передача данных далее get_mask_account или get_mask_card_number
-    соответственно, и возврат полного
- имени с маской"""
+    соответственно, и возврат полного имени с маской"""
     list_input_account_card = input_account_card.split(" ")
+    text_card = ""
+    for index in range(len(list_input_account_card) - 1):
+        text_card += list_input_account_card[index] + " "
     for name_card in list_input_account_card:
         if name_card.isdigit():
             if len(name_card) == 16:
-                return list_input_account_card(:-1) + get_mask_card_number(name_card)
+                return text_card + src.masks.get_mask_card_number(name_card)
             elif len(name_card) == 20:
-                return list_input_account_card(:-1) + get_mask_account(name_card)
+                return text_card + src.masks.get_mask_account(name_card)
             else:
                 return "error input"
     return "error input"
 
 
-def get_date(date_str:str)->str:
-    date_str=date_str.split("T")[0]
-    result_date = date_str.split("-")
-    return f"{result_date(2)}.{result_date(1)}.{result_date(0)}"
+def get_date(date_str: str) -> str:
+    """Перевод даты к необходимому формату"""
+    date_str = date_str.split("T")[0]
+    result_date: list[str] = date_str.split("-")
+    return f"{result_date[2]}.{result_date[1]}.{result_date[0]}"
