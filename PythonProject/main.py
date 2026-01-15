9759b8811027bfb73799530d25d8e9be0aa87348
Linad <linad43merzlikin@yandex.com>
2026-01-15 21:11:47 +0300

    edit import and add config linker

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -1,6 +1,6 @@
-import src.widget
+from src.widget import get_date, mask_account_card
 
-print(src.widget.mask_account_card("Visa Platinum 7000792289606361"))
-print(src.widget.mask_account_card("Maestro 7000792289606361"))
-print(src.widget.mask_account_card("Счет 73654108430135874305"))
-print(src.widget.get_date("2024-03-11T02:26:18.671407"))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))
