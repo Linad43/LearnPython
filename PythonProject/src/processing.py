3b3ce89c660dfeb69be3986c8f44b919daec1112
Linad <linad43merzlikin@yandex.com>
2026-01-19 14:29:19 +0300

    edit code

--- PythonProject/src/processing.py
+++ PythonProject/src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(input_list: list[map], state="EXECUTED") -> list[map]:
+def filter_by_state(input_list: list[dict[str, int | str]], state: str = "EXECUTED") -> list[dict[str, int | str]]:
     """Фильтр по состоянию, и возврат нового листа"""
     result_list = []
     for element in input_list:
@@ -7,6 +7,6 @@ def filter_by_state(input_list: list[map], state="EXECUTED") -> list[map]:
     return result_list
 
 
-def sort_by_date(input_list: list[map], reverse: bool = True) -> list[map]:
+def sort_by_date(input_list: list[dict[str, int | str]], reverse: bool = True) -> list[dict[str, int | str]]:
     """Сортировка по дате, и возврат нового листа"""
     return sorted(input_list, key=lambda p: p["date"], reverse=reverse)
