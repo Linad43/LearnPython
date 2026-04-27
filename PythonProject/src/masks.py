7fc5a4c89e0ef87312792d5fb685d2539b4f2a1b
Linad <merzlikindaniil97@gmail.com>
2026-04-27 14:46:32 +0300

    HM 12 2 done

--- PythonProject/src/masks.py
+++ PythonProject/src/masks.py
@@ -1,8 +1,17 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def get_list_card_numbers(input_number_card: str) -> list[str]:
     """получение листа номеров карточки по четыре"""
+    logger.debug(f"Start get_list_card_numbers(input_number_card:'{input_number_card}')")
     list_card = input_number_card.split(" ")
+    logger.debug(f"len(list_card):{len(list_card)}")
     if len(list_card) == 1:
+        logger.debug(f"list_card[0]: {list_card[0]}")
         if len(list_card[0]) != 16:
+            logger.critical(f"len(list_card[0]):{len(list_card[0])}!=16")
             return ["error input"]
         list_card = [
             list_card[0][:4],
@@ -10,17 +19,23 @@ def get_list_card_numbers(input_number_card: str) -> list[str]:
             list_card[0][8:12],
             list_card[0][12:],
         ]
+        logger.debug(f"get_list_card_numbers(input_number_card:'{input_number_card}') done")
     return list_card
 
 
 def get_mask_card_number(input_number_card: str) -> str:
     """получение маски карточки"""
+    logger.debug(f"Start get_mask_card_number(input_number_card:'{input_number_card}')")
     list_card = get_list_card_numbers(input_number_card)
+    logger.debug(f"get_mask_card_number(input_number_card:'{input_number_card}') done")
     return list_card[0] + " " + list_card[1][:2] + "** ****" + list_card[3]
 
 
 def get_mask_account(input_number_card: str) -> str:
     """получение маски номера счета"""
+    logger.debug("Start get_mask_account(input_number_card:'{input_number_card}')")
     if len(input_number_card) != 20:
+        logger.critical(f"len(input_number_card):{len(input_number_card)}!=20")
         return "error input"
+    logger.debug(f"get_mask_account(input_number_card:'{input_number_card}') done")
     return "**" + input_number_card[-4:]
