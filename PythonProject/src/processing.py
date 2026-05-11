924ad8af929cfca94ec4b21cc274b9b8110d7414
Linad <merzlikindaniil97@gmail.com>
2026-05-11 13:28:55 +0300

    add fun process_bank_search

--- PythonProject/src/processing.py
+++ PythonProject/src/processing.py
@@ -1,3 +1,11 @@
+import re
+import logging
+
+from pandas.core.computation.common import result_type_many
+
+logger = logging.getLogger(__name__)
+
+
 def filter_by_state(input_list: list[dict[str, int | str]], state: str = "EXECUTED") -> list[dict[str, int | str]]:
     """Фильтр по состоянию, и возврат нового листа"""
     result_list = []
@@ -10,3 +18,15 @@ def filter_by_state(input_list: list[dict[str, int | str]], state: str = "EXECUT
 def sort_by_date(input_list: list[dict[str, int | str]], reverse: bool = True) -> list[dict[str, int | str]]:
     """Сортировка по дате, и возврат нового листа"""
     return sorted(input_list, key=lambda p: p["date"], reverse=reverse)
+
+
+def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """Фильтр словарей, у которых в описании есть данная строка"""
+    logger.debug(f"Start process_bank_search(data, search:'{search}'")
+    pattern = re.compile(pattern=search, flags=re.IGNORECASE)
+    result: list[dict] = []
+    for element in data:
+        if re.search(pattern, element["description"]):
+            result.append(element)
+    logger.debug(f"End process_bank_search(data, search:'{search}'")
+    return result
